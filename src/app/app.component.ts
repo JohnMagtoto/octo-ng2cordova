2b192e2ee1a27af2b643c6b909f29d7b2d291060
JohnMagtoto <johnmagtoto@gmail.com>
2017-03-29 18:12:29 +0900

    master: partially working
    bridge between ng2 and cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app works!';
+  battery_level: number = 0;
+
+  constructor(private _zone: NgZone){}
+
+  ngOnInit(){
+    console.log("Debug: called ngOnInit");
+    window.addEventListener("batterystatus", (ev) =>{
+      this._zone.run(() => {
+        this.onBatteryStatus(ev);
+      })
+    }, false);
+  }
+
+  ngAfterViewInit(){
+    // console.log("Debug: called ngAfterViewInit");
+    // window.addEventListener("batterystatus", this.onBatteryStatus, false);
+  }
+
+  private onBatteryStatus(status){
+    console.log("battery level is : ", status.level);
+    this.battery_level = status.level;
+  }
 }
